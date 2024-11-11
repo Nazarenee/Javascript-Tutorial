600d6b470350bb9f6d74ce4ce510cbf12c871393
Nazarenee <cph-cm425@cphbusiness.dk>
2024-11-11 16:28:03 +0100

    new

---
+++ Javascript/Brocode-Tutorial/Array-folder/sort/index.js
@@ -0,0 +1,7 @@
+let fruits = ["apple","orange","banana","coconut"];
+
+fruits.sort();
+
+let numbers = [1,10,4,2,7,5,6,9];
+
+numbers.sort((a,b) => b - a );
---
+++ Javascript/Brocode-Tutorial/closure/index.js
@@ -0,0 +1,11 @@
+function outer(){
+    let messsage = "hello";
+    function inner(){
+    console.log(messsage);
+}
+inner();
+}
+
+
+
+outer();
